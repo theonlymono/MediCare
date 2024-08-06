3733e8a35b53e9378b4cf2e6ca33e215a95efcc2
Aung Kaung Myat <aung742004@gmail.com>
2024-08-06 23:07:53 +0630

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src//*.{html,js}", "./public/**/*.{html,js,php}", './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
